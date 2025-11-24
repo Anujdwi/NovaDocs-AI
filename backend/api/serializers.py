292c76c9b3f03b9aeb2000a53875e18c44046d50
Anujdwi <anujdwivedi00719@gmail.com>
2025-11-24 13:23:13 +0530

    Added all the necessary files

--- backend/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class IngestSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    file = serializers.FileField(required=False)
-    source_url = serializers.URLField(required=False)
